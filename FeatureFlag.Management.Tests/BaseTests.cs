6f56dc78e67b718e5170b18428e2826452b790c4
Felipe Baltazar <felipe.dasilvabaltazar@gmail.com>
2021-08-15 15:42:33 -0300

    🚀Unit tests has been created

---
+++ FeatureFlag.Management.Tests/BaseTests.cs
@@ -0,0 +1,38 @@
+﻿using FeatureFlag.Management.Tests.Helpers;
+using Moq;
+using System;
+using System.Collections.Generic;
+
+namespace FeatureFlag.Management.Tests
+{
+    public abstract class BaseTests
+    {
+        protected readonly Mock<IFeatureResolver> _featureResolver;
+        protected readonly Mock<IFeatureFlagManager> _featureFlagManager;
+
+        protected BaseTests()
+        {
+            _featureResolver = new Mock<IFeatureResolver>();
+            _featureFlagManager = new Mock<IFeatureFlagManager>();
+
+            _featureResolver.Setup(r => r.GetStringForKey(It.IsAny<string>(), It.IsAny<string>()))
+                            .Returns<string, string>((key, defaultValue) => defaultValue);
+
+            _featureResolver.Setup(r => r.GetBooleanForKey(It.IsAny<string>(), It.IsAny<bool>()))
+                            .Returns<string, bool>((key, defaultValue) => defaultValue);
+
+            _featureResolver.Setup(r => r.GetDoubleForKey(It.IsAny<string>(), It.IsAny<double>()))
+                            .Returns<string, double>((key, defaultValue) => defaultValue);
+
+            _featureResolver.Setup(r => r.GetNumberForKey(It.IsAny<string>(), It.IsAny<long>()))
+                            .Returns<string, long>((key, defaultValue) => defaultValue);
+
+            _featureResolver.Setup(r => r.DeserializeForKey(It.IsAny<string>(), It.IsAny<Func<List<string>>>()))
+                            .Returns<string, Func<List<string>>>((key, defaultValue) => defaultValue());
+
+            _featureFlagManager.Setup(m => m.Get<FakeFeature>())
+                               .Returns(new FakeFeature(_featureResolver.Object));
+        }
+
+    }
+}
---
+++ FeatureFlag.Management.Tests/FeatureFlagManagerTests.cs
@@ -0,0 +1,30 @@
+﻿using FeatureFlag.Management.Tests.Helpers;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using Xunit;
+
+namespace FeatureFlag.Management.Tests
+{
+    [Category("Manager")]
+    public class FeatureFlagManagerTests : BaseTests
+    {
+        public FeatureFlagManagerTests() : base()
+        {
+        }
+
+        [Fact(DisplayName = "Manager deve conseguir resolver nomes customizados para propriedades")]
+        public void Manager_Should_Resolve_Custom_Properties_Names()
+        {
+            var manager = new FeatureFlagManager(_featureResolver.Object);
+            var fakefeature = manager.Get<FakeFeature>();
+            var deserializedValue = JsonConvert.DeserializeObject<List<string>>(Constants.FAKE_FEATURE_DATA_VALUE);
+
+            Assert.Equal(deserializedValue.Count, fakefeature.Data.Count);
+            Assert.True(fakefeature.Data.All(d => deserializedValue.Contains(d)));
+            Assert.Equal(Constants.FAKE_FEATURE_IS_ENABLED_VALUE, fakefeature.IsEnabled);
+        }
+    }
+}
